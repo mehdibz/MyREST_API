feb7a7115ffb1ab8916963fe3c11f688955a3a93
mehdibz <bz.mehdi@gmail.com>
2019-08-24 00:52:47 -0700

    first commit

--- src/main/java/org/mbz/model/User.java
+++ src/main/java/org/mbz/model/User.java
@@ -7,9 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/*
- * This is our model class and it corresponds to User table in database
- */
 @Entity
 @Table(name="USER")
 public class User{
